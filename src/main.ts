cd7e4fa7de59df06452703c7f5450b4b8b0b8d66
Mansur Zakirov <mansur.zakirov@boolqa.com>
2020-08-24 13:24:14 +0300

    + update packages

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Axios from "axios";
 
+if (process.env.NODE_ENV === "development")
+  Axios.defaults.baseURL = "http://localhost:3001";
 Vue.config.productionTip = false;
 // Vue.config.errorHandler = function(err, vm, info) {
 //   alert(`Error: ${err.toString()}\nInfo: ${info}`);
@@ -12,5 +15,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
